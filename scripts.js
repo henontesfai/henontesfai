f7e71350e45b58dd5a8d39e986e3d8415966aa91
henontesfai <121614700+henontesfai@users.noreply.github.com>
2023-04-04 19:25:48 +0200

    res

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.getElementById('hamburger').addEventListener('click', function () {
 
 function downloadResume() {
   // Replace this URL with the actual resume file URL.
-  const resumeURL = 'https://example.com/resume.pdf';
+  const resumeURL = 'https://henontesfai.github.io/henontesfai/assets/Henon-resume.pdf';
   const link = document.createElement('a');
   link.href = resumeURL;
   link.download = 'resume.pdf';
@@ -18,6 +18,6 @@ function downloadResume() {
 
 function viewResume() {
   // Replace this URL with the actual resume file URL.
-  const resumeURL = 'https://example.com/resume.pdf';
+  const resumeURL = 'https://henontesfai.github.io/henontesfai/assets/Henon-resume.pdf';
   window.open(resumeURL, '_blank');
 };
